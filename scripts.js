34385ba240c92b989e7fb3e6998e22aaeae7f4e0
Mitchell Turner <turnerdistributed@gmail.com>
2014-05-06 17:05:06 -0400

    Added editable title added underlining on title

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 chrome.tabs.getSelected(null,function(tab) {
     var tablink = tab.url;
+    var title = tab.title;
+    document.getElementById('title').innerHTML = title;
     document.getElementById('url').value = tablink;
 });
